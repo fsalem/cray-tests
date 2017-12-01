b072338909ddc4974fea96145444f503d5dbce23
fsalem <farouqsalem@gmail.com>
2017-12-01 14:02:53 +0100

    display max bw

--- performance/multi-node/sum_rate_bandwidth.sh
+++ performance/multi-node/sum_rate_bandwidth.sh
@@ -4,7 +4,7 @@ FILE_NAME="jobs/$1"
 OUT_FILE_NAME="$FILE_NAME.rate.agg"
 echo '' > $OUT_FILE_NAME
 
-less $FILE_NAME | awk '
+awk '
   NR > 1 {count[$2]++}
   { sum[$2] += $4 }
   END {
@@ -12,4 +12,7 @@ less $FILE_NAME | awk '
       printf "%-15s\t%s\t%s\n", i,count[i], sum[i];
     }
   }
-' >> $OUT_FILE_NAME
+' $FILE_NAME >> $OUT_FILE_NAME
+
+
+awk -v max=0 '{if($3>max){want=$2; max=$3}}END{print want,max} ' $OUT_FILE_NAME
