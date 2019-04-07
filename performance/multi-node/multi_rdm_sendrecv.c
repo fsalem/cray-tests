ac3f27c8af62806534e8b4a27666910c0bd2fd40
bzcsalem <bzcsalem@das-pc1.zib.de>
2019-04-08 00:39:05 +0200

    multi send recv and comp event one by one

--- performance/multi-node/multi_rdm_sendrecv.c
+++ performance/multi-node/multi_rdm_sendrecv.c
@@ -160,6 +160,27 @@ static void cq_readerr(struct fid_cq *cq, const char *cq_str)
 	}
 }
 
+static int wait_for_comp_sync(struct fid_cq *cq, int num_completions)
+{
+	struct fi_cq_err_entry comp;
+	int ret;
+
+	while (num_completions > 0) {
+		ret = fi_cq_read(cq, &comp, 1);
+		if (ret > 0) {
+			num_completions--;
+		} else if (ret < 0 && ret != -FI_EAGAIN) {
+			if (ret == -FI_EAVAIL) {
+				cq_readerr(cq, "cq");
+			} else {
+				ct_print_fi_error("fi_cq_read", ret);
+			}
+			return ret;
+		}
+	}
+	return 0;
+}
+
 /*
  * fi_cq_err_entry can be cast to any CQ entry format.
  */
@@ -429,17 +450,25 @@ void *thread_fn(void *data)
 	int i, peer;
 	int size;
 	ssize_t __attribute__((unused))  fi_rc;
-	uint64_t t_start = 0, t_end = 0;
 	struct per_thread_data *ptd;
 	struct per_iteration_data it;
+	uint64_t t_start = 0, t_end = 0, remain_events;
 
 	it.data = data;
 	size = it.message_size;
+	current_message_size = it.message_size;
 
 	if (it.thread_id >= tunables.threads)
 		return (void *)-EINVAL;
 
 	ptd = &thread_data[it.thread_id];
+	ptd->bytes_sent = 0;
+	ptd->target_comp_events = loop * (numprocs - (numprocs / 2));
+
+	ct_tbarrier(&ptd->tbar);
+	int rc = MPI_Barrier(MPI_COMM_WORLD);
+	assert(rc == MPI_SUCCESS);
+	t_start = get_time_usec();
 
 #ifdef THREAD_SYNC
 	if (!it.thread_id)
@@ -447,46 +476,64 @@ void *thread_fn(void *data)
 	pthread_barrier_wait(&barr);
 #endif
 
-	if (myid == 0) {
-		peer = 1;
-		for (i = 0; i < loop + skip; i++) {
-			if (i == skip)
-				t_start = get_time_usec();
-
-			fi_rc = fi_tsend(ptd->ep, ptd->s_buf, size, NULL,
-					ptd->fi_addrs[peer], 0xDEADBEEF, NULL);
-			assert(!fi_rc);
-			wait_for_comp(ptd->scq, 1);
-
-			fi_rc = fi_trecv(ptd->ep, ptd->r_buf, size, NULL,
-					ptd->fi_addrs[peer], 0xDEADBEEF, 0, NULL);
-			assert(!fi_rc);
-			wait_for_comp(ptd->rcq, 1);
-		}
-
-		t_end = get_time_usec();
-	} else if (myid == 1) {
-		peer = 0;
-		for (i = 0; i < loop + skip; i++) {
-			fi_rc = fi_trecv(ptd->ep, ptd->r_buf, size, NULL,
-					ptd->fi_addrs[peer], 0xDEADBEEF, 0, NULL);
-			assert(!fi_rc);
-			wait_for_comp(ptd->rcq, 1);
-
-			fi_rc = fi_tsend(ptd->ep, ptd->s_buf, size, NULL,
-					ptd->fi_addrs[peer], 0xDEADBEEF, NULL);
-			assert(!fi_rc);
-			wait_for_comp(ptd->scq, 1);
+	if ((myid < (numprocs / 2))) {
+	    for (i = 0; i < loop; i++) {
+			uint64_t send_count = 0;
+			peer = (numprocs / 2);
+			while (peer < numprocs) {
+				fi_rc = fi_send(ptd->ep, ptd->s_buf, size, NULL,
+						ptd->fi_addrs[peer], NULL);
+				assert(!fi_rc);
+				wait_for_comp_sync(ptd->scq, 1);
+				send_count++;
+				ptd->bytes_sent += size;
+				peer++;
+			}
+			/*
+			remain_events = send_count;
+			while (remain_events > 0){
+				rc = wait_for_comp(ptd->scq,remain_events > 1000 ? 1000 :remain_events);
+				remain_events -= rc;
+				total_bytes_sent += rc * size;
+			}
+			*/
+	    }
+	}else{
+		for (i = 0; i < loop; i++) {
+			uint64_t recv_count = 0;
+			int end_peer = (numprocs / 2);
+			peer = 0;
+			while (peer < end_peer) {
+				fi_rc = fi_recv(ptd->ep, ptd->r_buf, size, NULL,
+						ptd->fi_addrs[peer], 0, NULL);
+				assert(!fi_rc);
+				wait_for_comp_sync(ptd->rcq, 1);
+				recv_count++;
+				peer++;
+			}
+			/*
+			remain_events = recv_count;
+			while (remain_events > 0){
+				rc = wait_for_comp(ptd->rcq,remain_events > 1000 ? 1000 :remain_events);
+				remain_events -= rc;
+			}
+			*/
 		}
 	}
+	t_end = get_time_usec();
+	ct_tbarrier(&ptd->tbar);
+	rc = MPI_Barrier(MPI_COMM_WORLD);
+	assert(rc == MPI_SUCCESS);
+
+	ptd->latency = (t_end - t_start) / (double) (loop);
+	ptd->time_start = t_start;
+	ptd->time_end = t_end;
 
 #ifdef THREAD_SYNC
 	if (!it.thread_id)
 		ctpm_Barrier();
 	pthread_barrier_wait(&barr);
 #endif
-
-	ptd->latency = (t_end - t_start) / (2.0 * loop);
 
 	return NULL;
 }
