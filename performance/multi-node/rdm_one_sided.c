063708f3a2da6e3aae9f71b0acde8efeb8e031a4
Farouk Salem <salem@zib.de>
2020-04-03 06:16:45 +0200

    remove pthread_barrier_t variable (which ignore threads options)

--- performance/multi-node/rdm_one_sided.c
+++ performance/multi-node/rdm_one_sided.c
@@ -119,7 +119,7 @@ struct per_iteration_data {
 };
 
 
-static pthread_barrier_t thread_barrier;
+/*static pthread_barrier_t thread_barrier;*/
 struct per_thread_data *thread_data;
 struct fi_info *fi, *hints;
 struct fid_fabric *fab;
@@ -610,7 +610,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	pthread_barrier_init(&thread_barrier, NULL, tunables.threads);
+	ctpm_Barrier();/*pthread_barrier_init(&thread_barrier, NULL, tunables.threads);*/
 
 	hints->ep_attr->type	= FI_EP_RDM;
 	hints->caps		= FI_MSG | FI_DIRECTED_RECV | FI_RMA;
--- performance/multi-node/tree_rdm_two_sided.c
+++ performance/multi-node/tree_rdm_two_sided.c
@@ -121,7 +121,7 @@ struct per_iteration_data {
 	};
 };
 
-static pthread_barrier_t thread_barrier;
+/*static pthread_barrier_t thread_barrier;*/
 struct per_thread_data *thread_data;
 struct fi_info *fi, *hints;
 struct fid_fabric *fab;
@@ -654,7 +654,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	pthread_barrier_init(&thread_barrier, NULL, tunables.threads);
+	/*pthread_barrier_init(&thread_barrier, NULL, tunables.threads);*/
 
 	hints->ep_attr->type = FI_EP_RDM;
 	hints->caps = FI_MSG | FI_DIRECTED_RECV | FI_RMA;
